0ec32592a0fad66ffc8cc71a1a5ce620e5c146a8
Janine Luk <janineluk95@gmail.com>
2018-11-13 11:19:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,20 +6,8 @@ def line(array)
     puts "The line is currently empty."
   else
   new_array.each.with_index(1) do |name, index|
-  new_array.push "#{index}. #{name}"
+  new_array.push("#{index}. #{name}")
 end
   puts "The line is currently: #{new_array.join("")}"
 end
-end
-
-def line(x)
-  line_array = []
-  if x.length == 0
-    puts "The line is currently empty."
-  else
-    x.each.with_index(1) do |name, index|
-      line_array.push("#{index}. #{name}")
-    end
-    puts "The line is currently: #{line_array.join(" ")}"
-  end
 end
