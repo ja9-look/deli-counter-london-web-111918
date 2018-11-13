eb4820a373f4ce21652d5181ed899dddb4904d01
Janine Luk <janineluk95@gmail.com>
2018-11-13 11:12:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.size == 0 
     puts "The line is currently empty."
   else
-    index = num + 2
+    index = number + 2
   array.each_with_index do |index, name|
   new_array << "#{index}. #{name} "
 end
